6055241329c178ae49262536134e203d1e8f5d94
joaofernandes52 <joaofernandes351@gmail.com>
2026-07-04 14:34:00 +0100

    security: fix mass assignment, replace findByIdAndRemove with findByIdAndDelete, async/await
    - Replace all 4 findByIdAndRemove calls with findByIdAndDelete (team, publications, activities, multimedia)
    - Replace all 5 findByIdAndUpdate(req.body) mass-assignment calls with explicit $set field lists
    - Convert all PUT and DELETE handlers to async/await with try/catch
    - Add verifyCsrf to all state-changing routes: 5 PUT, 4 DELETE, 3 POST (add_publication, add_activitie, add_multimedia)

--- REST/dashboard.js
+++ REST/dashboard.js
@@ -31,27 +31,20 @@ module.exports = function (app, ensureLoggedIn, verifyCsrf, upload, fs, path, pu
 
     });
 
-    app.delete("/team/:id", ensureLoggedIn('/login'), (req, res) => {
-        team_members.findByIdAndRemove(req.params.id, function (err) {
-            if (err) {
-                console.log(err);
-                res.redirect("/team");
-            } else {
-                res.redirect("/team");
-            }
-        });
+    app.delete("/team/:id", ensureLoggedIn('/login'), verifyCsrf, async (req, res, next) => {
+        try {
+            await team_members.findByIdAndDelete(req.params.id);
+            res.redirect("/team");
+        } catch (err) { next(err); }
     });
 
-    app.put("/edit_team_member/:id", ensureLoggedIn('/login'), (req, res) => {
-        team_members.findByIdAndUpdate(req.params.id, req.body, function (err, editMember) {
-            if (err) {
-                console.log(err);
-                res.redirect("/team")
-            } else {
-                res.redirect("/team");
-            }
-        });
-
+    app.put("/edit_team_member/:id", ensureLoggedIn('/login'), verifyCsrf, async (req, res, next) => {
+        try {
+            await team_members.findByIdAndUpdate(req.params.id, {
+                $set: { name: req.body.name, email: req.body.email, phone: req.body.phone, country: req.body.country, area: req.body.area, about: req.body.about }
+            });
+            res.redirect("/team");
+        } catch (err) { next(err); }
     });
 
     app.get("/add_publication", ensureLoggedIn('/login'), (req, res) => {
@@ -67,26 +60,18 @@ module.exports = function (app, ensureLoggedIn, verifyCsrf, upload, fs, path, pu
 
     });
 
-    app.post("/add_publication", ensureLoggedIn('/login'), (req, res) => {
-        publications.create(req.body, function (err, newPublication) {
-            if (err) {
-                console.log(err);
-                res.redirect("/publications")
-            } else {
-                res.redirect("/publications");
-            }
-        });
+    app.post("/add_publication", ensureLoggedIn('/login'), verifyCsrf, async (req, res, next) => {
+        try {
+            await publications.create({ title: req.body.title, authors: req.body.authors, type: req.body.type, date: req.body.date, abstract: req.body.abstract, url: req.body.url });
+            res.redirect("/publications");
+        } catch (err) { next(err); }
     });
 
-    app.delete("/publications/:id", ensureLoggedIn('/login'), (req, res) => {
-        publications.findByIdAndRemove(req.params.id, function (err) {
-            if (err) {
-                console.log(err);
-                res.redirect("/publications");
-            } else {
-                res.redirect("/publications");
-            }
-        });
+    app.delete("/publications/:id", ensureLoggedIn('/login'), verifyCsrf, async (req, res, next) => {
+        try {
+            await publications.findByIdAndDelete(req.params.id);
+            res.redirect("/publications");
+        } catch (err) { next(err); }
     });
 
     app.get("/edit_publication/:id", ensureLoggedIn('/login'), (req, res) => {
@@ -108,16 +93,13 @@ module.exports = function (app, ensureLoggedIn, verifyCsrf, upload, fs, path, pu
         });
     });
 
-    app.put("/edit_publication/:id", ensureLoggedIn('/login'), (req, res) => {
-        publications.findByIdAndUpdate(req.params.id, req.body, function (err, editPublication) {
-            if (err) {
-                console.log(err);
-                res.redirect("/publications")
-            } else {
-                res.redirect("/publications/" + req.params.id);
-            }
-        });
-
+    app.put("/edit_publication/:id", ensureLoggedIn('/login'), verifyCsrf, async (req, res, next) => {
+        try {
+            await publications.findByIdAndUpdate(req.params.id, {
+                $set: { title: req.body.title, authors: req.body.authors, type: req.body.type, date: req.body.date, abstract: req.body.abstract, url: req.body.url }
+            });
+            res.redirect("/publications/" + req.params.id);
+        } catch (err) { next(err); }
     });
 
     app.get("/add_activitie", ensureLoggedIn('/login'), (req, res) => {
@@ -133,26 +115,18 @@ module.exports = function (app, ensureLoggedIn, verifyCsrf, upload, fs, path, pu
 
     });
 
-    app.post("/add_activitie", ensureLoggedIn('/login'), (req, res) => {
-        activities.create(req.body, function (err, newActivitie) {
-            if (err) {
-                console.log(err);
-                res.redirect("/activities")
-            } else {
-                res.redirect("/activities");
-            }
-        });
+    app.post("/add_activitie", ensureLoggedIn('/login'), verifyCsrf, async (req, res, next) => {
+        try {
+            await activities.create({ title: req.body.title, participations: req.body.participations, abstract: req.body.abstract, date: req.body.date, url: req.body.url });
+            res.redirect("/activities");
+        } catch (err) { next(err); }
     });
 
-    app.delete("/activities/:id", ensureLoggedIn('/login'), (req, res) => {
-        activities.findByIdAndRemove(req.params.id, function (err) {
-            if (err) {
-                console.log(err);
-                res.redirect("/activities");
-            } else {
-                res.redirect("/activities");
-            }
-        });
+    app.delete("/activities/:id", ensureLoggedIn('/login'), verifyCsrf, async (req, res, next) => {
+        try {
+            await activities.findByIdAndDelete(req.params.id);
+            res.redirect("/activities");
+        } catch (err) { next(err); }
     });
 
     app.get("/edit_activitie/:id", ensureLoggedIn('/login'), (req, res) => {
@@ -174,16 +148,13 @@ module.exports = function (app, ensureLoggedIn, verifyCsrf, upload, fs, path, pu
         });
     });
 
-    app.put("/edit_activitie/:id", ensureLoggedIn('/login'), (req, res) => {
-        activities.findByIdAndUpdate(req.params.id, req.body, function (err, editActivitie) {
-            if (err) {
-                console.log(err);
-                res.redirect("/activities")
-            } else {
-                res.redirect("/activities/" + req.params.id);
-            }
-        });
-
+    app.put("/edit_activitie/:id", ensureLoggedIn('/login'), verifyCsrf, async (req, res, next) => {
+        try {
+            await activities.findByIdAndUpdate(req.params.id, {
+                $set: { title: req.body.title, participations: req.body.participations, abstract: req.body.abstract, date: req.body.date, url: req.body.url }
+            });
+            res.redirect("/activities/" + req.params.id);
+        } catch (err) { next(err); }
     });
 
     app.get("/add_multimedia", ensureLoggedIn('/login'), (req, res) => {
@@ -199,26 +170,18 @@ module.exports = function (app, ensureLoggedIn, verifyCsrf, upload, fs, path, pu
 
     });
 
-    app.post("/add_multimedia", ensureLoggedIn('/login'), (req, res) => {
-        multimedia.create(req.body, function (err, newMultimedia) {
-            if (err) {
-                console.log(err);
-                res.redirect("/multimedia")
-            } else {
-                res.redirect("/multimedia");
-            }
-        });
+    app.post("/add_multimedia", ensureLoggedIn('/login'), verifyCsrf, async (req, res, next) => {
+        try {
+            await multimedia.create({ title: req.body.title, description: req.body.description, url: req.body.url });
+            res.redirect("/multimedia");
+        } catch (err) { next(err); }
     });
 
-    app.delete("/multimedia/:id", ensureLoggedIn('/login'), (req, res) => {
-        multimedia.findByIdAndRemove(req.params.id, function (err) {
-            if (err) {
-                console.log(err);
-                res.redirect("/multimedia");
-            } else {
-                res.redirect("/multimedia");
-            }
-        });
+    app.delete("/multimedia/:id", ensureLoggedIn('/login'), verifyCsrf, async (req, res, next) => {
+        try {
+            await multimedia.findByIdAndDelete(req.params.id);
+            res.redirect("/multimedia");
+        } catch (err) { next(err); }
     });
 
     app.get('/edit_multimedia/:id', ensureLoggedIn('/login'), (req, res) => {
@@ -241,15 +204,13 @@ module.exports = function (app, ensureLoggedIn, verifyCsrf, upload, fs, path, pu
         });
     });
 
-    app.put("/edit_multimedia/:id", ensureLoggedIn('/login'), (req, res) => {
-        multimedia.findByIdAndUpdate(req.params.id, req.body, function (err, editMultimedia) {
-            if (err) {
-                console.log(err);
-                res.redirect("/multimedia")
-            } else {
-                res.redirect("/multimedia/");
-            }
-        });
+    app.put("/edit_multimedia/:id", ensureLoggedIn('/login'), verifyCsrf, async (req, res, next) => {
+        try {
+            await multimedia.findByIdAndUpdate(req.params.id, {
+                $set: { title: req.body.title, description: req.body.description, url: req.body.url }
+            });
+            res.redirect("/multimedia/");
+        } catch (err) { next(err); }
     });
 
     app.get('/edit_about_us/:id', ensureLoggedIn('/login'), (req, res) => {
@@ -267,15 +228,13 @@ module.exports = function (app, ensureLoggedIn, verifyCsrf, upload, fs, path, pu
         });
     });
 
-    app.put("/edit_about_us/:id", ensureLoggedIn('/login'), (req, res) => {
-        about_us.findByIdAndUpdate(req.params.id, req.body, function (err, editAbout_us) {
-            if (err) {
-                console.log(err);
-                res.redirect("/brant")
-            } else {
-                res.redirect("/brant/");
-            }
-        });
+    app.put("/edit_about_us/:id", ensureLoggedIn('/login'), verifyCsrf, async (req, res, next) => {
+        try {
+            await about_us.findByIdAndUpdate(req.params.id, {
+                $set: { objectives: req.body.objectives, mission: req.body.mission, structural_org: req.body.structural_org, about: req.body.about, address: req.body.address, email: req.body.email, phone1: req.body.phone1, phone2: req.body.phone2, facebook: req.body.facebook, twitter: req.body.twitter, youtube: req.body.youtube }
+            });
+            res.redirect("/brant/");
+        } catch (err) { next(err); }
     });
 
     app.post('/add_team_member', ensureLoggedIn('/login'), upload.single('image'), async (req, res, next) => {
